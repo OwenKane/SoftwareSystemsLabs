a7b433034582e9281e5e4627a6863518a0a57b15
OwenKane <owen.a.kane@gmail.com>
2017-01-27 14:51:00 +0000

    Area of rect complete

--- Lab1/Exe2/main.c
+++ Lab1/Exe2/main.c
@@ -5,18 +5,12 @@
 #include <stdio.h>
 #include <string.h>
 #include "areaOfRectangle.h"
+#include <stdlib.h>
 
 int main(int argc, char **argv){
-    int num;
-    sscanf (argv[2],"%d",&num);
 
-    int num2;
-    sscanf (argv[3],"%d",&num2);
-
-    printf("The length of input arg[1]  is: %zu\n", strlen(argv[1]));
-
-    if(strcmp(argv[1], "Rectangle")) {
-        areaOfRectangle(num,num2);
+    if(strcmp(argv[1],"Rectangle") == 0) {
+        areaOfRectangle(atoi(argv[2]),atoi(argv[3]));
     }
 }
 
