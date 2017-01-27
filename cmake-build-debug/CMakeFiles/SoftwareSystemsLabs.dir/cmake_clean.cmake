5412c3ab82fc92d8a14cc0de140bd5330551c7c3
OwenKane <owen.a.kane@gmail.com>
2017-01-27 13:39:24 +0000

    Using CLion

---
+++ cmake-build-debug/CMakeFiles/SoftwareSystemsLabs.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SoftwareSystemsLabs.dir/Lab1/hello.c.o"
+  "SoftwareSystemsLabs.pdb"
+  "SoftwareSystemsLabs"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SoftwareSystemsLabs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
