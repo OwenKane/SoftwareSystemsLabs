bbf199301c1dbf43f548d392928e9526e0cb4f21
OwenKane <owen.a.kane@gmail.com>
2017-02-09 14:26:18 +0000

    Added make_starter files and reformatted

---
+++ Lab2/Part2/Make_Starter/area_of_triangle.h
@@ -0,0 +1,7 @@
+#ifndef SOFTWARESYSTEMSLABS_AREAOFTRIANGLE_H_
+#define SOFTWARESYSTEMSLABS_AREAOFTRIANGLE_H_
+
+area_of_triangle( double a, double b, double c );
+
+#endif //  SOFTWARESYSTEMSLABS_AREAOFTRIANGLE_H_
+
